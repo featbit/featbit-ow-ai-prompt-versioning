29829d5323887838ba1115c228d38d93a4fcbb47
Comiscience <hu-beau@outlook.com>
2025-01-16 19:29:44 +0800

    first upload

---
+++ prompt-version-control-online/src/components/first/header.tsx
@@ -0,0 +1,37 @@
+import { Icons } from "@/components/icons";
+import { MobileDrawer } from "@/components/mobile-drawer";
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
+export function Header() {
+  return (
+    <header className="sticky top-0 h-[var(--header-height)] z-50 p-0 bg-background/60 backdrop-blur">
+      <div className="flex justify-between items-center container mx-auto p-2">
+        <Link
+          href="https://www.featbit.co"
+          title="brand-logo"
+          className="relative mr-6 flex items-center space-x-2"
+        >
+          <Icons.logo className="w-auto" />
+          <span className="font-semibold text-lg">FeatBit</span>
+        </Link>
+        <div className="hidden lg:block">
+          <Link
+            href="#"
+            className={cn(
+              buttonVariants({ variant: "default" }),
+              "h-8 text-primary-foreground rounded-lg group tracking-tight font-medium"
+            )}
+          >
+            Get Started
+          </Link>
+        </div>
+        <div className="mt-2 cursor-pointer block lg:hidden">
+          <MobileDrawer />
+        </div>
+      </div>
+      <hr className="absolute w-full bottom-0" />
+    </header>
+  );
+}
